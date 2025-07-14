2c394e330f7ceece686c5c90cebb79dbd37dd87d
Sharp0802 <sharp0802.business@gmail.com>
2025-07-14 12:11:56 +0900

    :sparkles: Declare stream for StreamHandle

---
+++ dfl/include/dfl/io/SafeHandleStream.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "io/SafeHandle.h"
+#include "io/Stream.h"
+
+namespace dfl {
+  class SafeHandleStream : public Stream {
+    SafeHandle _handle;
+
+  protected:
+    explicit SafeHandleStream(SafeHandle&& handle) : _handle(std::move(handle)) {}
+
+  public:
+    ~SafeHandleStream() override = default;
+
+    bool poll(std::chrono::milliseconds timeout) override;
+    ssize_t write(std::span<u8> src) override;
+    ssize_t read(std::span<u8> dst) override;
+    void flush() override;
+  };
+}

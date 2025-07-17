f1f174ec0fe4c4864101f3807914649c80e356b9
Sharp0802 <sharp0802.business@gmail.com>
2025-07-17 22:45:27 +0900

    :sparkles: Add a way to validate pipe

--- dfl/include/dfl/net/NamedPipeServerStream.h
+++ dfl/include/dfl/net/NamedPipeServerStream.h
@@ -1,10 +1,27 @@
 #pragma once
 
+#include <stop_token>
+
 #include "dfl/net/NamedPipeStream.h"
 
 namespace dfl {
   class NamedPipeServerStream final : public NamedPipeStream {
+#ifdef __unix__
+    SafeHandle _listener;
+#endif
+
   public:
     explicit NamedPipeServerStream(const std::string &name);
+
+    [[nodiscard]]
+    bool operator!() const override {
+#ifdef __unix__
+      return !_listener;
+#else
+      return true;
+#endif
+    }
+
+    bool wait_for_connection(std::chrono::milliseconds timeout);
   };
 }

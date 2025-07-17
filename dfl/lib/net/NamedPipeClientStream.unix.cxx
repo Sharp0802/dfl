3c0a973b63eabffb05a7200085735fe7aa1e82ed
Sharp0802 <sharp0802.business@gmail.com>
2025-07-17 22:47:46 +0900

    :recycle: Use UDS instead of FIFO

--- dfl/lib/net/NamedPipeClientStream.unix.cxx
+++ dfl/lib/net/NamedPipeClientStream.unix.cxx
@@ -3,16 +3,27 @@
 #include "net/NamedPipeClientStream.h"
 
 #include <fcntl.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
+#include <sys/un.h>
 
 #include "io/SafeHandle.h"
 
 namespace {
-  dfl::SafeHandle OpenNamedPipe(const std::string &name) {
+  dfl::SafeHandle connect_to_socket(const std::string &name) {
     const auto path = std::format("/tmp/{}", name);
 
-    const auto fd = open(path.c_str(), O_RDWR);
+    const int fd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (fd < 0) {
+      return dfl::SafeHandle::Invalid();
+    }
+
+    sockaddr_un addr = {};
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, path.c_str(), sizeof(addr.sun_path) - 1);
+
+    if (::connect(fd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) < 0) {
+      close(fd);
       return dfl::SafeHandle::Invalid();
     }
 
@@ -22,7 +33,7 @@ namespace {
 
 namespace dfl {
   NamedPipeClientStream::NamedPipeClientStream(const std::string &name)
-    : NamedPipeStream(name, OpenNamedPipe(name)) {
+    : NamedPipeStream(name, connect_to_socket(name)) {
   }
 }
 
--- dfl/lib/net/NamedPipeServerStream.unix.cxx
+++ dfl/lib/net/NamedPipeServerStream.unix.cxx
@@ -2,31 +2,83 @@
 
 #include "net/NamedPipeServerStream.h"
 
-#include <fcntl.h>
-#include <sys/stat.h>
+#include <cstring>
+#include <sys/poll.h>
+#include <sys/socket.h>
+#include <sys/un.h>
 
 #include "io/SafeHandle.h"
 
 namespace {
-  dfl::SafeHandle CreateNamedPipe(const std::string &name) {
+  dfl::SafeHandle create_listen_socket(const std::string &name) {
     const auto path = std::format("/tmp/{}", name);
 
-    if (mkfifo(path.c_str(), 0666) < 0 && errno != EEXIST) {
+    const int listen_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (listen_fd < 0) {
       return dfl::SafeHandle::Invalid();
     }
 
-    const auto fd = open(path.c_str(), O_RDWR);
-    if (fd < 0) {
+    sockaddr_un addr = {};
+    addr.sun_family = AF_UNIX;
+    strncpy(addr.sun_path, path.c_str(), sizeof(addr.sun_path) - 1);
+
+    unlink(path.c_str());
+
+    if (bind(listen_fd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) < 0) {
+      close(listen_fd);
       return dfl::SafeHandle::Invalid();
     }
 
-    return dfl::SafeHandle(fd);
+    if (listen(listen_fd, SOMAXCONN) < 0) {
+      close(listen_fd);
+      return dfl::SafeHandle::Invalid();
+    }
+
+    return dfl::SafeHandle(listen_fd);
   }
 }
 
 namespace dfl {
   NamedPipeServerStream::NamedPipeServerStream(const std::string &name)
-    : NamedPipeStream(name, CreateNamedPipe(name)) {
+    : NamedPipeStream(name, SafeHandle::Invalid()),
+      _listener(create_listen_socket(name)) {
+  }
+
+  bool NamedPipeServerStream::wait_for_connection(std::chrono::milliseconds timeout) {
+    pollfd pfd{
+      .fd = _listener.native_handle(),
+      .events = POLLIN,
+      .revents = 0
+    };
+
+    int timeout_int = static_cast<int>(timeout.count());
+    if (timeout.count() > INT_MAX) {
+      dfl_warning(timeout.count() > INT_MAX, "NamedPipeServerStream::wait_for_connection(): timeout is too big");
+      timeout_int = INT_MAX;
+    } else if (timeout.count() < 0) {
+      timeout_int = INT_MAX;
+    }
+
+    const auto ret = ::poll(&pfd, 1, timeout_int);
+    if (ret < 0) {
+      return false;
+    }
+    if (ret == 0) {
+      errno = 0;
+      return false;
+    }
+    if (~pfd.revents & POLLIN) {
+      errno = EIO;
+      return false;
+    }
+
+    const int fd = accept(_listener.native_handle(), nullptr, nullptr);
+    if (fd < 0) {
+      return false;
+    }
+
+    _handle = std::move(SafeHandle(fd));
+    return true;
   }
 }
 
